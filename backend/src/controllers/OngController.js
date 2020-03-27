5caaace6bc05931e15e021f5ebde7978abeb2631
Gabriel Dias Bahniuk <gabriel.dias-bahniuk@t-systems.com>
2020-03-27 15:47:23 +0100

    ESLint + EditorConfig + Prettier configured

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,26 +1,26 @@
-const connection = require('../database/connection');
-const crypto = require('crypto');
+const connection = require("../database/connection");
+const crypto = require("crypto");
 
 module.exports = {
-    async index(req, res) {
-        const ongs = await connection('ongs').select('*');
+  async index(req, res) {
+    const ongs = await connection("ongs").select("*");
 
-        return res.json(ongs);
-    },
-    async create(req, res) {
-        const { name, email, whatsapp, city, state } = req.body;
+    return res.json(ongs);
+  },
+  async create(req, res) {
+    const { name, email, whatsapp, city, state } = req.body;
 
-        const id = crypto.randomBytes(4).toString('HEX');
+    const id = crypto.randomBytes(4).toString("HEX");
 
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            state
-        })
+    await connection("ongs").insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      state
+    });
 
-        return res.json({ id });
-    }
-}
+    return res.json({ id });
+  }
+};
