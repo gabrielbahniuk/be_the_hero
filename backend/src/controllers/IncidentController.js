16548f9ee6de110afd0a61a2f0e90eb60572f307
Gabriel Dias Bahniuk <gabriel.dias-bahniuk@t-systems.com>
2020-03-28 18:37:38 +0100

    Bugfix backend

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -26,28 +26,28 @@ module.exports = {
     return res.json(incidents)
   },
   async create (req, res) {
-    const ongId = req.headers.authorization
+    const ong_id = req.headers.authorization
     const { title, description, value } = req.body
 
     const [id] = await connection('incidents').insert({
       title,
       description,
       value,
-      ongId
+      ong_id
     })
 
     return res.json({ id })
   },
   async delete (req, res) {
     const { id } = req.params
-    const ongId = req.headers.authorization
+    const ong_id = req.headers.authorization
 
     const incident = await connection('incidents')
       .where('id', id)
       .select('ong_id')
       .first()
 
-    if (incident.ong_id !== ongId) {
+    if (incident.ong_id !== ong_id) {
       return res.status(401).json({ error: 'Operation not permitted.' })
     }
     await connection('incidents')
