c7f533f5addbeba02a8014d909cd35a432dceddc
Gabriel <gabrieldiasbnu@hotmail.com>
2020-04-12 18:12:08 +0200

    Fix #1

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,28 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiPower, FiTrash2 } from 'react-icons/fi';
-import logoImage from '../../assets/logo.svg';
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
+import logoImage from "../../assets/logo.svg";
 
-import api from '../../services/api';
-import './styles.css';
-
+import api from "../../services/api";
+import "./styles.css";
 
 export default function Profile() {
   const [incidents, setIncidents] = useState([]);
 
   const history = useHistory();
 
-  const ongName = localStorage.getItem('ongName');
-  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem("ongName");
+  const ongId = localStorage.getItem("ongId");
 
   useEffect(() => {
-    api.get('profile', {
-      headers: {
-        Authorization: ongId,
-      },
-    }).then((response) => {
-      setIncidents(response.data);
-    });
+    api
+      .get("profile", {
+        headers: {
+          Authorization: ongId,
+        },
+      })
+      .then((response) => {
+        setIncidents(response.data);
+      });
   }, [ongId]);
 
   async function handleDeleteIncident(id) {
@@ -34,25 +35,25 @@ export default function Profile() {
       });
       setIncidents(incidents.filter((incident) => incident.id !== id));
     } catch (err) {
-      alert('Problem removing case. Please try again');
+      alert("Problem removing case. Please try again");
     }
   }
 
   function handleLogout() {
     localStorage.clear();
 
-    history.push('/');
+    history.push("/");
   }
 
   return (
     <div className="profile-container">
       <header>
         <img src={logoImage} alt="Be the Hero" />
-        <span>
-          {`Welcome, ${ongName}`}
-        </span>
+        <span>{`Welcome, ${ongName}`}</span>
 
-        <Link className="button" to="/incidents/new">Register new case</Link>
+        <Link className="button" to="/incidents/new">
+          Register new case
+        </Link>
         <button onClick={handleLogout} type="button">
           <FiPower size={18} color="#e02041" />
         </button>
@@ -60,7 +61,6 @@ export default function Profile() {
 
       <h1>Registered cases</h1>
       <ul>
-
         {incidents.map((incident) => (
           <li key={incident.id}>
             <strong>CASE:</strong>
@@ -68,9 +68,18 @@ export default function Profile() {
             <strong>DESCRIPTION:</strong>
             <p>{incident.description}</p>
             <strong>VALUE:</strong>
-            <p>{Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}</p>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL",
+              }).format(incident.value)}
+            </p>
             <button type="button">
-              <FiTrash2 onClick={() => handleDeleteIncident(incident.id)} size={20} color="#a8a8b3" />
+              <FiTrash2
+                onClick={() => handleDeleteIncident(incident.id)}
+                size={24}
+                color="#a8a8b3"
+              />
             </button>
           </li>
         ))}
